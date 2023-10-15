6c2aefe7e1f47ac1a83352669a6c82190b57afc5
Evan Glasscock <121138977+evanglass@users.noreply.github.com>
2023-10-15 10:53:04 -0700

    Update homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <div class="button-group">
-    <h1>Welcome to GroupSync</h1>
+    <h1 style="margin-bottom: 5px;">Welcome to GroupSync</h1>
+    <img src="images/groupSyncLogo.png" alt="Logo" height="100" style="margin-bottom: 20px;"/>
     <a href="/calendar">Calendar</a>
     <a href="/friends">Friends</a>
     <a href="/profile">Profile</a>
