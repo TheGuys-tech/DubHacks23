0bdfa9fdbeea98249c518b69a2a37e8d97732080
Evan Glasscock <121138977+evanglass@users.noreply.github.com>
2023-10-15 06:25:00 -0700

    Add friends add/remove

--- src/routes/friends/+page.server.js
+++ src/routes/friends/+page.server.js
@@ -1,4 +1,4 @@
-import {MongoClient} from "mongodb"
+import {MongoClient, ObjectId} from "mongodb"
 import { MONGO_URL } from "$env/static/private"
 
 export async function load({ params }) {
@@ -25,4 +25,81 @@ export async function load({ params }) {
         users: JSON.parse(JSON.stringify(results ,(key,value) => key === '_id'? value.toString(value) : value))
     }
 
+}
+
+export const actions = {
+    default: async ({request}) => {
+        let formData = await request.formData();
+        let client = new MongoClient(MONGO_URL);
+        try {
+            await client.connect();
+        }
+        catch (error)
+        {
+            return {postData: "Could not connect to MongoDB."};
+        }
+        
+        let db = client.db("groupSync");
+        let col = db.collection("users");
+        
+        let me = 0;
+        let results = await col.find().toArray();
+        let jsonData = JSON.parse(JSON.stringify(results ,(key,value) => key === '_id'? value.toString(value) : value))
+        
+        try {
+            if (formData.get("action") == "remove")
+            {
+                let newFriendsSrc = jsonData[me].friends.filter((friend) => friend != formData.get("friendId"))
+                let newFriendsDst = jsonData.filter((user) => user._id == formData.get("friendId"))[0].friends.filter((friend) => friend != jsonData[me]._id)
+                
+                col.updateOne(
+                    {"_id": new ObjectId(jsonData[me]._id)},
+                    { "$set": 
+                        {"friends": newFriendsSrc}
+                    }
+                )
+                col.updateOne(
+                    {"_id": new ObjectId(formData.get("friendId"))},
+                    { "$set":
+                        {"friends": newFriendsDst}
+                    }
+                )
+            } 
+            else if (formData.get("action") == "add")
+            {
+                let newFriendsSrc = jsonData[me].friends
+                let newFriendsDst = jsonData.filter((user) => user._id == formData.get("friendId"))[0].friends
+                if (newFriendsSrc.indexOf(formData.get("friendId")) < 0)
+                {
+                    newFriendsSrc.push(formData.get("friendId"));
+                }
+                if (newFriendsDst.indexOf(jsonData[me]._id) < 0)
+                {
+                    newFriendsDst.push(jsonData[me]._id)
+                }
+
+                col.updateOne(
+                    {"_id": new ObjectId(jsonData[me]._id)},
+                    { "$set": 
+                        {"friends": newFriendsSrc}
+                    }
+                )
+                col.updateOne(
+                    {"_id": new ObjectId(formData.get("friendId"))},
+                    { "$set":
+                        {"friends": newFriendsDst}
+                    }
+                )
+            }
+            
+            return {
+                postData: "Success"
+            }
+        } 
+        catch (error) {
+            return {
+                postData: "Error"
+            }
+        }
+    }
 }
--- src/routes/friends/+page.svelte
+++ src/routes/friends/+page.svelte
@@ -1,9 +1,10 @@
 <script>
     export let data;
+    export let postData;
+    let me = 0;
     let friends = [];
     if (!data.error)
     {
-        let me = 0;
         friends = data.users[me].friends.map((id) => data.users.filter((u) => u._id == id)[0]);
     }
 </script>
@@ -12,7 +13,53 @@
 {#if data.error}
 <div>{data.error_msg}</div>
 {:else}
-{#each friends as friend}
-<div>{friend.name}</div>
-{/each}
+{#if postData != undefined}
+<div style="background-color: burlywood; width: 100%;">{postData}</div>
 {/if}
+{#each friends as friend}
+<div class="user-profile-card">
+    <div class="user-profile-card-namebox">
+        <img class="user-profile-icon" src="images/user-icon.png" alt="User profile icon"/>
+        <p class="user-profile-card-name">{friend.name}</p>
+    </div>
+    <form action="/friends" method="POST">
+        <input type="hidden" name="action" value="remove" />
+        <input type="hidden" name="friendId" value="{friend._id}" />
+        <input type="submit" value="Remove friend" />
+    </form>
+</div>
+{/each}
+<br>
+<form action="/friends" method="POST">
+    <input type="hidden" name="action" value="add" />
+    <input name="friendId" />
+    <input type="submit" value="Add friend" />
+</form>
+<p>Your FriendID: {data.users[me]._id}</p>
+{/if}
+
+<style>
+    .user-profile-card {
+        width: 300px;
+        height: 100px;
+        display: flex;
+        flex-direction: column;
+        border: 2px solid black;
+    }
+
+    .user-profile-card-namebox {
+        display: flex;
+        align-items: center;
+    }
+
+    .user-profile-card-name {
+        width: max-content;
+        font-size: 1.5em;
+        margin: 5px;
+    }
+
+    .user-profile-icon {
+        width: 64px;
+        height: 64px;
+    }
+</style>
